8425dc0f4cc98382fb0c89fd069e07bba2aebc33
avicienna99 <nadim95@hotmail.com>
2024-12-04 14:23:28 +0100

    Update main.go

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	})
 
 	// Start the web server on port 8080
-	port := ":8080"
+	port := ":80"
 	fmt.Printf("Starting server on http://localhost%s\n", port)
 	if err := http.ListenAndServe(port, nil); err != nil {
 		fmt.Printf("Error starting server: %v\n", err)
